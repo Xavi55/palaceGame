955ea3f8067017ad60a9d346bd1ee5da379322cc
xavi55 <evinxavier5@gmail.com>
2021-02-15 18:39:08 -0500

    server logic

--- server.py
+++ server.py
@@ -11,11 +11,43 @@ clients={} #{<room>:{'x':<name>,'o':<name>}}
 
 @app.route('/')
 def index():
-    return ('<h1>TEST</h1>')
+    return render_template('index.html')
+#########################
+@socketio.on('makeRoom')
+def handle(data):
+    name=data['name']
+    room=data['room']
 
+    if room in clients:
+        emit('err',{'mess':'room {} already exists'.format(room)})
+    else:
+        join_room(room)
+        clients[room]=Game()#new game
+        clients[room].addPlayer(name)
+        #clients[room]={'p{}'.format(len(game.players)):name}
+        emit('joined',{'mess':'player joined'})
+        status(room)
+@socketio.on('joinRoom')
+def handle(data):
+        name=data['name']
+        room=data['room']
 
-
-################################3
+        if room in clients:
+            if len(clients[room].players) == 4:
+                emit('err',{'mess':'room {} is full'.format(room)})
+            else:
+                join_room(room)
+                clients[room].addPlayer(name)
+                #clients[room]={'p{}'.format(len(game.players)):name}
+                emit('joined',{'mess':'player joined'})
+                status(room)
+        else:
+            emit('err',{'mess':'room {} does not exist'.format(room)})
+##########################
+def status(room):
+    print('total rooms',clients)
+    print('players in room: {} - {}'.format(room,len(clients[room].players)))
+##########################
 if __name__ == '__main__':
     socketio.run(app,debug=True)
 
